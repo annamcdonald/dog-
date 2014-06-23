9b8d56e9b88bd144c2141d3ab1e647961fe2d1c2
annamcdonald <annamcd04@gmail.com>
2014-06-23 13:46:09 -0400

    made buddy the dog

---
+++ dog.rb
@@ -0,0 +1,48 @@
+class Dog
+
+	def initialize(name, breed, leg_count)
+		@name= buddy
+		@breed= golden_retriever
+		@leg_count = 4
+	end
+
+
+	def bark
+		puts "WOOF WOOF :)"
+	end
+
+	def name
+		@name 
+	end
+
+	def name=(name_argument)
+		@name=name_argument
+	end
+
+
+	def breed
+		@breed 
+
+	end
+
+	def breed=(breed_argument)
+		@breed=breed_argument
+
+	end	
+
+
+
+	def leg_count
+		@leg_count
+	end
+
+
+	def leg_count=(leg_count_argument)
+		@leg_count=leg_count_argument
+	end
+end
+
+
+
+
+#def name=
